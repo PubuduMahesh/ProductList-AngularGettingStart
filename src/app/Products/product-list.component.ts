c4cfbab576020002677434562e60fc7308f1eae9
Pubudu Meththananda <Pubudu.Meththananda@cambio.lk>
2019-07-17 01:09:24 +0100

    add getters ands setters for a listFilter attribute and set filter for the table

--- src/app/Products/product-list.component.ts
+++ src/app/Products/product-list.component.ts
@@ -13,7 +13,23 @@ export class ProductListComponent implements OnInit{
 	imageWidth: number = 50;
 	imageMargin: number = 2;
 	showImage: boolean = false;
-	listFilter: string = 'cart';
+	
+	filteredProductList: IProduct[] = [];
+
+	constructor(){
+		this.filteredProductList = this.products;
+		this.listFilter = 'cart';
+	}
+
+	_listFilter: string;
+	get listFilter():string{
+		return this._listFilter;
+	}
+
+	set listFilter(item:string){console.log('ksusslfa');
+		this._listFilter = item;
+		this.filteredProductList = this._listFilter ? this.performFilter(this._listFilter):this.products;
+ 	}
 
 	products: IProduct[]=[
 	  {
@@ -68,6 +84,11 @@ export class ProductListComponent implements OnInit{
 	  }
   ];
 
+  performFilter(filterBy:string): IProduct[]{
+  	filterBy = filterBy.toLowerCase();
+  	return this.products.filter((product:IProduct)=>product.productName.toLowerCase().indexOf(filterBy) !== -1);
+  }
+
   toggleImage(): void {
   	this.showImage = !this.showImage;
   }
@@ -75,4 +96,5 @@ export class ProductListComponent implements OnInit{
   ngOnInit(): void{
   	console.log('on init method fired');
   }
+  
 }
