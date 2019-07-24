5a31d19eef4fbca4c6e9d8e15258d415bcccaccb
Pubudu Meththananda <Pubudu.Meththananda@cambio.lk>
2019-07-24 23:27:18 +0100

    http error handling

--- src/app/Products/product.service.ts
+++ src/app/Products/product.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {catchError,tap} from 'rxjs/operators';
 
 import {IProduct} from './product';
 
@@ -9,9 +10,32 @@ import {IProduct} from './product';
 })
 export class ProductService{
 	private productUrl = 'api/products/products.json';
+
 	constructor(private http:HttpClient){}
 	
 	getProducts():Observable<IProduct[]>{
-		return this.http.get<IProduct[]>(this.productUrl);
+		return this.http.get<IProduct[]>(this.productUrl).pipe(
+			tap(data => console.log('All: '+Json.stringfy(data))),
+			catchError(this.handleError)
+		);
+	}
+
+	private handleError(err:HttpErrorResponse){
+		//in a real world app, we may send the server to some remote logging instrastructure.
+		//instead of just logging it to the console.
+
+		let errorMessage = '';
+
+		if(err.error instanceof ErrorEvent){
+			//A client-side or network error occurred. Handle it acordingly. 
+			errorMessage = 'An error occurred: $(err.error.message)';
+		}else{
+			//The backend returned an unsuccessful response code.
+			//the response body may contain clues as to what went wrong,
+
+			errorMessage = 'Server returned code: ${err.status}, error message is: ${err.message}'
+		}
+		console.error(errorMessage);
+		return throwError(errorMessage);
 	}
 }
