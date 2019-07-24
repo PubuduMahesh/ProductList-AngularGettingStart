b386693e7cd15aa552a7143fc126eaa42793fe3f
Pubudu Meththananda <Pubudu.Meththananda@cambio.lk>
2019-07-24 23:07:22 +0100

    injecting http client service

--- src/app/Products/product.service.ts
+++ src/app/Products/product.service.ts
@@ -1,4 +1,6 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 import {IProduct} from './product';
 
@@ -6,6 +8,8 @@ import {IProduct} from './product';
 	providedIn: 'root'
 })
 export class ProductService{
+
+	constructor(private http:HttpClient){}
 	
 	getProducts():IProduct[]{
 		return [
